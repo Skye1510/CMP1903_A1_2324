39d71be6c9fdb239fb35a7ed34a904ed29bdea7b
Skye1510 <145765958+Skye1510@users.noreply.github.com>
2024-04-09 12:09:39 +0100

    Fixed sevens out
    replaced lazy effective solution with a more efficient solution

--- CMP1903_A1_2324/SevensOut.cs
+++ CMP1903_A1_2324/SevensOut.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,58 +11,56 @@ namespace CMP1903_A1_2324
     {
         int playerOneScore = 0;
         int playerTwoScore = 0;
-        int turn = 1;
         int rounds = 1;
 
         public List<int> PlayGame()
         {
-            bool done = false;
-            while(!done)
+            bool done = false; //controls game loop
+            bool isPlayerTwo = false; //bool used to state which players turn it is
+            while(!done) //while not done
             {
-                if(turn == 1)
+
+                int scoreToAdd = 0; //variable used to hold the score that needs to be added to the correct players score
+                if (!isPlayerTwo) //if its player ones turn
                 {
-                    Console.WriteLine("Round " + rounds + "! Fight!");
-                    Console.WriteLine("Player 1's turn. Press any key to roll dice.");
-                    Console.ReadKey();
-                    List<int> list = RollDie(2);
-                    Console.WriteLine("\nRolled a " + list[0] + " and a " + list[1]);
-                    if (list[0] + list[1] == 7)
-                    {
-                        done = true;
-                    }
-                    else if (list[0] == list[1])
-                    {
-                        playerOneScore += 2 * (list[0] + list[1]);
-                    }
-                    else
-                    {
-                        playerOneScore += list[0] + list[1];
-                    }
-                    turn = 2;
+                    Console.WriteLine("Round " + rounds + "! Fight!"); //print out which round it is
                 }
-                else
+
+                Console.WriteLine("Player " + (Convert.ToInt32(isPlayerTwo) + 1) + "'s turn. Press any key to roll dice."); //state which players turn it is + provide instruction
+                Console.ReadKey(); //wait for the player to press a key
+                List<int> list = RollDie(2); //get a list of 2 random numbers from the RollDie() function
+                Console.WriteLine("\nRolled a " + list[0] + " and a " + list[1]); //state what the player rolled
+
+                if (list[0] + list[1] == 7) //if the sum of both rolls is a 7
                 {
-                    Console.WriteLine("Player 2's turn. Press any key to roll dice.");
-                    Console.ReadKey();
-                    List<int> list = RollDie(2);
-                    Console.WriteLine("\nRolled a " + list[0] + " and a " + list[1]);
-                    if (list[0] + list[1] == 7)
-                    {
-                        done = true;
-                    }
-                    else if (list[0] == list[1])
-                    {
-                        playerTwoScore += 2 * (list[0] + list[1]);
-                    }
-                    else
-                    {
-                        playerTwoScore += list[0] + list[1];
-                    }
-                    turn = 1;
-                    rounds++;
+                    done = true; //end the game
+                    continue; //skip the rest of the code
                 }
-                Console.WriteLine("Player One's score: " + playerOneScore + "\nPlayer Two's score: " + playerTwoScore);
+                else if (list[0] == list[1]) //if both rolls are the same number
+                {
+                    scoreToAdd = 2 * (list[0] + list[1]); //add them together, double the result then assign the value to scoreToAdd
+                }
+                else //otherwise
+                {
+                    scoreToAdd = list[0] + list[1]; //just add them together then assign to scoreToAdd
+                }
+
+                if(!isPlayerTwo) //if its player ones turn
+                {
+                    playerOneScore += scoreToAdd; //increase player ones score by scoreToAdd
+                }
+                else // othereise its player twos turn
+                {
+                    playerTwoScore += scoreToAdd; //so increase player twos score by scoreToAdd
+                    rounds++; //increment number of rounds
+                }
+
+                isPlayerTwo = !isPlayerTwo;//flip which players turn it is
+
+                Console.WriteLine("Player One's score: " + playerOneScore + "\nPlayer Two's score: " + playerTwoScore); //let the players know the score
             }
+
+            //once the game is over, report the winner
             if(playerOneScore > playerTwoScore)
             {
                 Console.WriteLine("Player One wins!");
@@ -75,6 +74,7 @@ namespace CMP1903_A1_2324
                 Console.WriteLine("Tie!");
             }
 
+            //return stats back to the game object
             List<int> ints = new List<int>();
             ints.Add(playerOneScore);
             ints.Add(playerTwoScore);
--- CMP1903_A1_2324/ThreeOrMore.cs
+++ CMP1903_A1_2324/ThreeOrMore.cs
@@ -8,5 +8,18 @@ namespace CMP1903_A1_2324
 {
     internal class ThreeOrMore : Game
     {
+        int playerOneScore = 0;
+        int playerTwoScore = 0;
+        int turn = 1;
+        int rounds = 1;
+
+        /*public List<int> PlayGame()
+        {
+            bool done = false;
+            while(!done)
+            {
+
+            }
+        }*/
     }
 }
